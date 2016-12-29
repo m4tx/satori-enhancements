81fcd38b4c94f6da2940cc639fed99ad0db00eb1
Mateusz Maćkowski <m4tx@m4tx.pl>
2016-12-29 15:01:52 +0100

    Fix sorting of "(1.0)"-style points in ranking

--- ext/js/ranking.js
+++ ext/js/ranking.js
@@ -13,6 +13,11 @@
             if (!num || num.length === 0 || isNaN(parseFloat(num[0]))) {
                 return text;
             }
+            if (text.indexOf('(' + num[0] + ')') !== -1) {
+                // e.g. "(1.0)" - indicates number of points granted for solving
+                // problem (but not the actual number of points one has got)
+                return (parseFloat(num[0]) - 1000).toString();
+            }
             return num[0];
         }
     });
