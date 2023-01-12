85e95fe408518e6dec86e2a940413d8cf5bc6bdc
Dominik Korsa <dominik.korsa@gmail.com>
2023-01-12 20:57:26 +0100

    Disable submit button when input invalid

--- ext/js/submit.js
+++ ext/js/submit.js
@@ -4,26 +4,31 @@
     $('#content table tr:nth-child(1) th').text('Problem:');
     $('#content table tr:nth-child(2) th').text('File:');
     $('#content table').append(
-        '<tr><th>Code:</th><td colspan="2"><textarea id="code-textarea"></textarea><td></tr>'
+        '<tr><th>Code:</th><td colspan="2"><textarea id="code-textarea" tabindex="3"></textarea><td></tr>'
     );
 
     const problemSelect = $('#id_problem');
     const filePicker = $('#id_codefile');
     const codeTextarea = $('#code-textarea');
     const form = $('#content form');
+    const submitButton = $('#content form input[type=submit]');
+
+    submitButton.attr('tabindex', '4');
+
+    let loading = false;
 
     const updatePickers = () => {
         const fileSelected = filePicker.val() !== '';
         const textEntered = codeTextarea.val() !== '';
         codeTextarea.attr('disabled', fileSelected);
         filePicker.attr('disabled', textEntered);
+        submitButton.attr('disabled', loading || !problemSelect.val() || !(textEntered || fileSelected));
     }
 
+    problemSelect.on('change', updatePickers);
     filePicker.on('change', updatePickers);
     codeTextarea.on('input', updatePickers);
     updatePickers();
-
-    let loading = false;
 
     form.on('submit', async (event) => {
         event.preventDefault();
@@ -39,22 +44,22 @@
             formData.set('codefile', blob, 'code.cpp');
         } else return;
         loading = true;
+        updatePickers();
         try {
             const response = await fetch(form.attr('action'), {
                 method: 'POST',
                 body: formData,
             });
-            if (!response.ok) {
-                alert("Bład: " + response.status)
-                console.log(await response.text());
-                loading = false;
+            if (response.ok) {
+                window.location = response.url;
                 return;
             }
-            window.location = response.url;
+            alert("Bład: " + response.status);
         } catch (error) {
             alert("Błąd: " + error.message);
             console.error(error);
         }
         loading = false;
+        updatePickers();
     });
 })();
