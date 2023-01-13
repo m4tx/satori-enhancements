3ab34aa1318c670bffcd56a8a6d458556bcd6d4a
Dominik Korsa <dominik.korsa@gmail.com>
2023-01-13 19:56:24 +0100

    Fix problem list resetting

--- ext/js/problem.js
+++ ext/js/problem.js
@@ -2,7 +2,6 @@
     'use strict';
 
     const { contestID, problemID } = getContestAndProblemID(document.location.href);
-    console.log(contestID, problemID);
     $('<a class="button">Results</a>')
         .attr('href', `${SATORI_URL_HTTPS}contest/${contestID}/results?results_filter_problem=${problemID}`)
         .appendTo('#content > .buttton_bar')
