5c98bcd8dbf11f997937cf758ed93292f40f3c74
Dominik Korsa <dominik.korsa@gmail.com>
2023-06-24 18:29:17 +0200

    Fix gulp watch task and remove dead shouldRedirectToSubmit code (#41)
    * Fix gulp.watch
    
    * Remove dead shouldRedirectToSubmit code

--- ext/js/results-list.js
+++ ext/js/results-list.js
@@ -1,15 +1,6 @@
 (function () {
     'use strict';
 
-    browser.runtime.sendMessage({
-        action: 'shouldRedirectToSubmit',
-    }).then(shouldRedirect => {
-        if (shouldRedirect) {
-            document.location = $('div#content table.results tbody > ' +
-                'tr:nth-child(2) > td:first > a').attr('href');
-        }
-    });
-
     const contestID = getContestID(document.location.href);
 
     function addCompareSubmitsButton() {
